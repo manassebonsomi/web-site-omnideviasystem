85dff824b3b851db48b778124583db4d0b3a509b
manassebonsomi <manassebonsomi.bmm@gmail.com>
2026-05-08 02:57:19 +0200

    Mise à jour

--- js/script.js
+++ js/script.js
@@ -29,3 +29,57 @@ function revealElements() {
 // EXECUTION
 window.addEventListener("scroll", revealElements);
 revealElements();
+
+
+// LOADER
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+  }, 1500);
+});
+
+// DARK MODE
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+});
+
+// FAQ
+const faqItems = document.querySelectorAll(".faq-item");
+faqItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
+
+// AOS
+AOS.init({
+  duration:1200
+});
+
+// GSAP
+gsap.from(".hero-content", {
+  y:100,
+  opacity:0,
+  duration:1.5
+});
+
+// COUNTER
+const counters = document.querySelectorAll(".stat-box h3"); 
+counters.forEach(counter => {
+  const updateCounter = () => {
+    const target =
+    +counter.innerText.replace('+','');
+    let count = +counter.innerText;
+    const increment = target / 100;
+    if(count < target){
+      counter.innerText =
+      `${Math.ceil(count + increment)}+`;
+      setTimeout(updateCounter,20);
+    }
+  }
+
+  counter.innerText = "0";
+  updateCounter();
+  
+});
